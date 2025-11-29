16eb8ea11e0d01f8cc1678b63054588f508f27f3
Tomas Meriles <tomasmeriles17@gmail.com>
2025-11-29 19:22:15 -0300

    fix(nest): use logical delete

--- nest/src/modules/user/user.repository.ts
+++ nest/src/modules/user/user.repository.ts
@@ -6,11 +6,11 @@ import {
   UserFindUniqueArgs,
   UserUpdateInput,
 } from '../../../prisma/generated/models.js';
-import { ReadRepository } from '../../shared/interface/repository/read.repository.js';
 import { PrismaService } from '../prisma/prisma.service.js';
+import { CRUDRepository } from '../../shared/interface/repository/crud.repository.js';
 
 @Injectable()
-export class UserRepository implements ReadRepository {
+export class UserRepository implements CRUDRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll(args?: UserFindManyArgs) {
@@ -34,6 +34,10 @@ export class UserRepository implements ReadRepository {
   }
 
   async delete(id: number) {
-    return this.prisma.user.delete({ where: { id } });
+    return this.prisma.user.update({ where: { id }, data: { active: false } });
+  }
+
+  async reactivate(id: number) {
+    return this.prisma.user.update({ where: { id }, data: { active: true } });
   }
 }
--- nest/src/shared/interface/repository/crud.repository.ts
+++ nest/src/shared/interface/repository/crud.repository.ts
@@ -1,7 +1,15 @@
-export interface ReadRepository {
+export interface CRUDRepository {
   findAll(select: object, where: object, orderBy: object): Promise<object>;
 
   findFirst(select: object, where: object): Promise<object | null>;
 
   findUnique(select: object, where: object): Promise<object | null>;
+
+  create(data: object): Promise<object>;
+
+  update(id: number, data: object): Promise<object>;
+
+  delete(id: number): Promise<object>;
+
+  reactivate(id: number): Promise<object>;
 }
